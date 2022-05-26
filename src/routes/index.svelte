b371761b1f4e25dc5c2288ab90f21b52adcb69da
Chris Herrmann <cherrmann@lemon-digital.com>
2022-05-26 13:34:25 +0200

    copy update

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -2,5 +2,5 @@
     import "../app.css";
 </script>
 
-<h1 class="text-3xl font-bold underline">Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<h1 class="text-3xl font-bold underline">Svelte Tailwind Boilerplate</h1>
+<p>Lightning fast, highly optimised pages Google will love</p>
